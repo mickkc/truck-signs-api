2971bc2ae0fa6fbfa8780e2f5ab3e4dfb4067059
mickkc <contact@mickkc.dev>
2026-08-03 17:15:28 +0200

    feat: update entrypoint script
    - Added a delay between database connection checks
    - Migrate the database after a successfull connection
    - Create a superuser if it does not already exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,21 @@ echo "Waiting for postgres to connect ..."
 
 # Wait for the database to be up and ready, if not ready, then sleep for 5 seconds
 while ! nc -z db 5432; do
-  #TODO: Add missing implementation
+  sleep 5
 done
 
 echo "PostgreSQL is active"
 
 python manage.py collectstatic --noinput
 
-#TODO add migrations
+python manage.py migrate
 echo "Postgresql migrations finished"
+
+# Create a superuser using the specified credentials. If the user already exists, an error code would be returned.
+# Thet's why there is '|| true', it make the command not return an error code, because otherwise the container would
+# fail to start when a superuser already exists.
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
 
 gunicorn tsa_app.wsgi:application --bind 0.0.0.0:8000
