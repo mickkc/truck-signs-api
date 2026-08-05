f12903e5300c34dd796069b360946dfa8a44937e
mickkc <contact@mickkc.dev>
2026-08-05 17:33:34 +0200

    fix: update entrypoint script
    - Enter /app/src to correctly resolve manage.py and gunicorn module
    - Fix database waiting argument
    - Pass username and email to manage.py createsuperuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 echo "Waiting for postgres to connect ..."
+cd /app/src
 
 # Wait for the database to be up and ready, if not ready, then sleep for 5 seconds
-while ! nc -z ${DB_HOST} ${$DB_PORT}; do
+while ! nc -z ${DB_HOST} ${DB_PORT}; do
   sleep 5
 done
 
@@ -19,7 +20,8 @@ echo "Postgresql migrations finished"
 # Thet's why there is '|| true', it make the command not return an error code, because otherwise the container would
 # fail to start when a superuser already exists.
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    python manage.py createsuperuser --no-input || true
+    echo "Creating superuser ${DJANGO_SUPERUSER_USERNAME}..."
+    python manage.py createsuperuser --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --no-input || true
 fi
 
 gunicorn tsa_app.wsgi:application --bind 0.0.0.0:8000
